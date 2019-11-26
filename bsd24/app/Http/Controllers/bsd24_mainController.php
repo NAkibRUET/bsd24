9a7ce35166498545f0deaf5b03318d20be646f78
Nakib Hossin <hussain0296@gmail.com>
2019-11-27 05:57:53 +0600

    Login & Sign Up Backend

--- bsd24/app/Http/Controllers/bsd24_mainController.php
+++ bsd24/app/Http/Controllers/bsd24_mainController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class bsd24_mainController extends Controller
 {
     function bsd24_home_page()
@@ -25,6 +25,69 @@ class bsd24_mainController extends Controller
     function reviews()
     {
         return view('bsd24_reviews');
+    }
+    public function sign_up_request(request $data)
+    {
+        $fullName= $data->fullName; 
+        $userEmail= $data->userEmail; 
+        $mobile= $data->mobile;
+        $password= $data->password;
+        $msg = "";
+        $status;
+        if(!empty($fullName) && !empty($userEmail) && !empty($mobile) &&!empty($password)){
+            $exist = DB::table('bsd_user_informations')->where('user_email',$userEmail)->get()->count();
+            if(!$exist){
+                $values = array('full_name' => $fullName,'user_email' => $userEmail, 'user_phone' => $mobile, 'password' => $password);
+                $insert = DB::table('bsd_user_informations')->insert($values);
+
+                if($insert){
+                    $status = "ok";
+                }
+                else{
+                    $status = "failed";
+                }
+            }
+            else{
+                $status = "failed";
+                $msg = "An account with this Email already exists";    
+            }
+        }
+        else{
+            $status = "failed";
+            $msg = "No field should be empty!";
+        }
+        $arr = array('a' => $status, 'b' => $msg);
+
+        echo json_encode($arr);
+    }
+    public function login_request(request $data){
+        $userEmail= $data->userEmail;
+        $password= $data->password;
+        $msg = "";
+        $exist = DB::table('bsd_user_informations')->where('user_email',$userEmail)->get()->count();
+        if(!$exist){
+            $status = "failed";
+            $msg = "Username not found!";    
+        }
+        else{
+            $match = DB::table('bsd_user_informations')->where('user_email',$userEmail)->where('password',$password)->get()->count();
+            if($match){
+                $status = "ok";
+                $email = DB::table('bsd_user_informations')->where('user_email',$userEmail)->value('user_email');
+                $name = DB::table('bsd_user_informations')->where('user_email',$userEmail)->value('full_name');
+                $data = array('name' => $name, 'email' => $email);
+                session(['user_info'=>$data]);
+
+            }
+            else{
+                $status = "failed";
+                $msg = "Password doesn't match!";           
+            }
+        }
+    
+        $arr = array('a' => $status, 'b' => $msg);
+
+        echo json_encode($arr);
     }
     
 }
--- bsd24/resources/views/bsd24_signup.blade.php
+++ bsd24/resources/views/bsd24_signup.blade.php
@@ -6,48 +6,44 @@
 
 	<!--SECTION STARTS HERE -->
 	<!--Please, place all your div/box/anything inside the above SECTION-->
-
+	
 	<div class="section" style="">
 		<div class="box40">
 		<!--For Alert Messages Use the Following-->
-			<!--Error / UnSuccessful Event
 			
-			 <div class="alertDanger">
-			  	<i class="fa fa-times"></i> Type The Message Here
+			
+			 <div class="alertDanger" id="alertDanger" style="display: none;">
+			 </div>
+			
+			 <div class="alertSuccess" style="display: none;" id="alertSuccess">
 			 </div>
 
-			-->
-
-			<!--Successful Event / Success Message
-
-			 <div class="alertSuccess">
-			  	<i class="fa fa-check"></i> Type The Message Here
-			 </div>
-
-		-->
+		
 			 <h5 class="boxHeader">Sign Up:</h5>
-			 <form method="post">
-			 	<label for="nameOfUser">Name:</label>
-			 	<input required id="nameOfUser" type="text" name="nameOfUser" placeholder="Enter your name" class="form-control" autocomplete="off">
-			 	<br>
-			 	<label for="email">Email:</label>
-			 	<input required id="email" type="email" name="email" placeholder="Enter your email" class="form-control" autocomplete="off">
-			 	<br>
-			 	<label for="mobile">Mobile No:</label>
-			 	<input required id="mobile" type="text" name="mobile" placeholder="Enter your mobile no" class="form-control" autocomplete="off">
-			 	<br>
-			 	<label for="password">Password:</label>
+			 <form id="signUpForm">
+			 	<input type="hidden" id="login_token" name="_token" value="{{csrf_token()}}">
+                <label for="nameOfUser">Name:*</label>
+			 	<input required id="nameOfUser" type="text" name="nameOfUser" placeholder="Enter your name" class="form-control" autocomplete="off" onkeyup="validator(this)">
+			 	
+			 	<label for="userEmail">Email:</label>
+			 	<input required id="userEmail" type="email" name="email" placeholder="Enter your email" class="form-control" autocomplete="off" onfocusout="validator(this)">
+			 	
+			 	<label for="mobile">Mobile No:*</label>
+			 	<input required id="mobile" type="text" name="mobile" placeholder="Enter your mobile no" class="form-control" autocomplete="off" onfocusout="validator(this)">
+			 	
+			 	
+			 	<label for="password">Password:*</label>
 			 	<input required id="password" type="password" name="password" placeholder="Enter your password"  class="form-control">
-			 	<br>
-			 	<label for="RetypePassword">Re-Type Password:</label>
-			 	<input required id="RetypePassword" type="password" name="RetypePassword" placeholder="Enter your password again"  class="form-control">
-			 	<br>
+			 	
+			 	<label for="RetypePassword">Confirm Password:*</label>
+			 	<input required id="RetypePassword" type="password" name="RetypePassword" placeholder="Enter your password again"  class="form-control" onchange="validator(this)">
+			 	
 			 	<div class="user-option">
 					<div class="checkbox">
 						<label><input type="checkbox" name="remember_me" value="yes"> I agree to the <a href="#">Term & Conditions</a></label>
 					</div>
 				</div>
-				<br>
+				
 			 	<button type="submit" id="signUpBtn" class="btn btn-success">Create Account</button>
 			</form>
 		</div>
@@ -55,5 +51,124 @@
 	</div>
 	<!--Please, place all your div/box/anything inside the above SECTION-->
 
+<script type="text/javascript">
+
+const pat1 = new RegExp(/(\%27)|(\')|(\-\-)|(\%23)|(#)/i);
+const pat2 = new RegExp(/[1-9][0-9]*|0/);
+const pat3 = new RegExp(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/);
+const pat4 = new RegExp(/(^(\+88|0088)?(01|09){1}[56789]{1}(\d){8})$/);
+const pat5 = new RegExp(/\S+@\S+\.\S+/);
+function validator(inputData){
+	if(inputData.id=="nameOfUser"){
+		let name = document.getElementById('nameOfUser').value;
+		if(name != ""){
+			if(pat1.test(name) || pat2.test(name) || pat3.test(name)) {
+				document.getElementById('alertDanger').style.display = 'block';
+				document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Invalid Name';
+			}
+			else{
+				document.getElementById('alertDanger').style.display = 'none';
+			}
+		}
+		else{
+			document.getElementById('alertDanger').style.display = 'none';
+		}
+	}
+	else if(inputData.id=="mobile"){
+		let mobile = document.getElementById('mobile').value;
+		if(mobile!=""){
+			pat4.test(mobile)?document.getElementById('alertDanger').style.display = 'none':document.getElementById('alertDanger').style.display = 'block';
+				document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Invalid Mobile Number';	
+		}
+		else{
+			document.getElementById('alertDanger').style.display = 'none';
+		}
+	}
+	else if(inputData.id=="userEmail"){
+		let userEmail = document.getElementById('userEmail').value;	
+		if(userEmail!=""){
+			pat5.test(userEmail)?document.getElementById('alertDanger').style.display = 'none':document.getElementById('alertDanger').style.display = 'block';
+				document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Invalid Email Address';	
+		}
+		else{
+			document.getElementById('alertDanger').style.display = 'none';
+		}
+	}
+
+}
+$(document).ready(function(){
+  $("#signUpBtn").click(function(event){
+    event.preventDefault();
+    var nameError = 0;
+    var mobileError = 0;
+    var emailError = 0;
+    var fullName = $('#nameOfUser').val();
+    var userEmail = $('#userEmail').val();
+    var mobile = $('#mobile').val();
+    var password = $('#password').val();
+    var RetypePassword = $('#RetypePassword').val();
+   
+    if(pat1.test(fullName) || pat2.test(fullName) || pat3.test(fullName)) {
+		nameError = 1;	
+	}
+	if(pat4.test(mobile)) {
+		
+	}
+	else{
+		mobileError = 1;	
+	}
+    if(password === RetypePassword && nameError == 0 && mobileError == 0){
+        $.ajax({
+            method: "POST",
+            url : '/sign_up/request',
+            dataType:"json",
+            data :{
+            	_token: $('#login_token').val(),
+            	fullName: fullName, 
+            	userEmail: userEmail, 
+            	mobile: mobile, 
+            	password: password,  
+            },
+            success: function(response){
+                console.log(response.a);
+                
+                if(response.a=="ok")
+                {
+                	document.getElementById('alertDanger').style.display = 'none';
+                    document.getElementById('alertSuccess').style.display = 'block';
+					document.getElementById('alertSuccess').innerHTML = '<i class="fa fa-check"></i> Successfully Registered, Please Login.';
+                }
+                else if(response.a=="failed")
+                {
+                	document.getElementById('alertDanger').style.display = 'block';
+                	document.getElementById('alertSuccess').style.display = 'none';
+                	response.b == ""?document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Something went wrong, Please try again'
+					:document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"> '+response.b;	 
+                }
+            },
+            error: (error)=>{
+            	console.log(JSON.stringify(error));
+            } 
+        })
+		document.getElementById('alertDanger').style.display = 'none';			
+    }
+    else{
+    	if(nameError == 1){
+	    	document.getElementById('alertDanger').style.display = 'block';
+			document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Invalid Name';
+		}
+		else if(mobileError == 1){
+			document.getElementById('alertDanger').style.display = 'block';
+			document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Invalid Mobile';	
+		}
+		else{
+			document.getElementById('alertDanger').style.display = 'block';
+			document.getElementById('alertDanger').innerHTML = '<i class="fa fa-times"></i> Password and Confirm Password should be same';	
+		}
+    }
+  });
+});
+
+</script>
 
 @endsection
