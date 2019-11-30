15723e741f6dab7ae081b0f968e802eacafe8017
Nakib Hossin <hussain0296@gmail.com>
2019-11-30 20:31:02 +0600

    Review Image Slider in home page

--- bsd24/app/Http/Controllers/bsd24_mainController.php
+++ bsd24/app/Http/Controllers/bsd24_mainController.php
@@ -8,7 +8,8 @@ class bsd24_mainController extends Controller
 {
     function bsd24_home_page()
     {
-        return view('main_home_page');
+        $review = DB::table('bsd_reviews')->where('status','1')->orderBy('id', 'DESC')->take(10)->get();
+        return view('main_home_page')->with('review',$review);
     }
     function login()
     {
